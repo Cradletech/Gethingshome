6038c670505d9d487239d7a64b38cdcb6f67442c
project- <football.ashish@gmail.com>
2014-08-29 15:07:27 +0530

    GTH V1- Initial Test commit

--- src/com/example/getthingshome/MainActivity.java
+++ src/com/example/getthingshome/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listofitems);
 				//activity_main);
-		System.out.println("Hello Android");
+		System.out.println("Hello Android   ");
 	}
 
 	@Override
